fc1851b068818728f8e5a6ca6e5470327438124a
Dave Dopson (epic) <ddopson@epicapplications.com>
2012-05-06 00:06:17 -0700

    initial project boilerplate

---
+++ src/glympse/demo/GlympseActivity.java
@@ -0,0 +1,13 @@
+package glympse.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GlympseActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
